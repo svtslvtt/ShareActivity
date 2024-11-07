ee2e2f3c82328768e412301a6ddc2a3513830033
svtslvtt <tup60276@temple.edu>
2024-11-07 12:01:34 -0500

    Implemented intent

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,14 +1,22 @@
 package edu.temple.funwithintents
 
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    val sendIntent = Intent(Intent.ACTION_SEND)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
@@ -16,6 +24,18 @@ class MainActivity : AppCompatActivity() {
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
 
+            val sendIntent = Intent()
+            sendIntent.action = Intent.ACTION_SEND
+            sendIntent.putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+            sendIntent.type = "text/plain"
+
+            if (sendIntent.resolveActivity(packageManager) != null) {
+                startActivity(sendIntent)
+
+            }
         }
     }
+
+
+
 }
